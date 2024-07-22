92d8405a7b0beccbd6f4321e24b888c005575e08
redatman <evicwork@gmail.com>
2024-07-22 23:14:54 +0800

    Refactor: Rename app to django_model_server
    Rename the app to reflect its functionality as a Django model server. Add a TODO for supporting full Django projects.

--- utils.py
+++ utils.py
@@ -35,7 +35,8 @@ def init_db(db_file: str = "db.sqlite3"):
 
 def init_django(settings_module: str = "settings.dev"):
 
-    # Init settings method 1: With settings file(Supports full django project)
+    # Init settings method 1: With settings file
+    # TODO: Supports full django project?
     # allow async
     os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
